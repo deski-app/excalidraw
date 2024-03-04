186636e8c5bb2871d15a8c2a69c08110e48845b9
ParkalotDev3 <parkalotdev3@ParkalotDev3s-MacBook-Air.local>
2024-03-04 12:30:24 +0100

    fix wrong lock change

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ import cssVariables from "./css/variables.module.scss";
 import { AppProps } from "./types";
 import { ExcalidrawElement, FontFamilyValues } from "./element/types";
 import { COLOR_PALETTE } from "./colors";
-import { PARKALOT_LOCK_STATE } from "./utils";
 
 export const isDarwin = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
 export const isWindows = /^Win/.test(navigator.platform);
@@ -335,7 +334,7 @@ export const DEFAULT_ELEMENT_PROPS: {
   strokeStyle: "solid",
   roughness: ROUGHNESS.artist,
   opacity: 100,
-  locked: PARKALOT_LOCK_STATE,
+  locked: false,
 };
 
 export const LIBRARY_SIDEBAR_TAB = "library";
